e2eb6513e24f733073fb292a40baf655d19a68d6
Jordan Smellie <jsmellie@gmail.com>
2014-07-18 09:52:09 -0400

    Added DataTypes to Timesheet

--- EmployeeTimeclock/EmployeeTimeclock/Models/Timesheet.cs
+++ EmployeeTimeclock/EmployeeTimeclock/Models/Timesheet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace EmployeeTimeclock.Models
     public class Timesheet
     {
         public int TimesheetId { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
+        [DataType(DataType.Time)]
         public DateTime TimeIn { get; set; }
+
+        [DataType(DataType.Time)]
         public DateTime TimeOut { get; set; }
+
         public int DailyHours { get; set; }
         
         public virtual Employee Employee { get; set; }
